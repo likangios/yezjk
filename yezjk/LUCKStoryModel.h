daa883eb1874c71a2ab5d8034b683afd6414c812
luck <likangios@163.com>
2018-06-25 08:30:26 +0800

    no message

--- yezjk/LUCKStoryModel.h
+++ yezjk/LUCKStoryModel.h
@@ -9,5 +9,23 @@
 #import <Foundation/Foundation.h>
 
 @interface LUCKStoryModel : NSObject
+/**
+ id
+ */
+@property(nonatomic,strong) NSString *picId;
+/**
+ 标题
+ */
+@property(nonatomic,strong) NSString *title;
+/**
+ 内容
+ */
+@property(nonatomic,strong) NSString *content;
+
+/**
+ 图片类型 1=爸爸故事，2=妈妈故事，
+ */
+@property(nonatomic,strong) NSNumber *type;
+
 
 @end
